b83cbf2a7cafcd615799c644aae59f2a36b2c92c
zexe <zexe063@gmail.com>
2023-11-23 09:47:33 +0530

    mongodb is connected

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,34 @@
 
 const express = require("express");
 const server = express();
+const mongoose = require("mongoose");
 const cors = require("cors");
 server.use(cors({
     origin:"https://verce-test-djrl.vercel.app"
-}));
+}))
 server.use(express.json())
 
-const arr = []
+mongoose.connect("mongodb://localhost:27017/verceltes").then(()=>{
+    console.log("database connected")
+}).catch(()=>{
+    console.log("eroro")
+})
+
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+})
+const user = mongoose.model("user", userSchema);
+
 
 server.get("/", (req,res)=>{
     res.send("helo muraru")
 })
 
-server.post("/home",(req,res)=>{
-
-   arr.push(req.body)
-   res.send(arr)
+server.post("/home",async(req,res)=>{
+const data  = await new user(req.body);
+data.save()
+res.json(data);
 })
 server.listen(8000,()=>{
     console.log("started")
